8b7307582d210eae95ebe6bf40d141131df7a261
sxiao1 <sxiao1@stuy.edu>
2019-04-04 00:26:01 -0400

    changed a bit

--- Radix.java
+++ Radix.java
@@ -1,67 +1,55 @@
 import java.util.*;
 public class Radix{
-  public static void radixsort(int[] data){
-    @SuppressWarnings("unchecked")
-    MyLinkedList<Integer>[] bucket = new MyLinkedList[20];
-    for(int i =0; i < bucket.length; i++){
-      bucket[i] = new MyLinkedList();
-    }
-    MyLinkedList<Integer> tempB = new MyLinkedList();
-    for(int i = 0; i < data.length; i++){
-      tempB.add(data[i]);
-    }
-    for(int i = 0; i <data.length; i++){
-      if(data[i] < 0){
-        bucket[9 + data[i] % 10].addBeg(data[i]);
-      }
-      else{
-        bucket[10 + data[i] % 10].addEnd(data[i]);
-      }
-    }
-    int maxNum = 0;
-    int maxInd = 0;
-    for(int i = 0; i < data.length; i++){
-      if(i == 0){
-        maxNum = data[i];
-        maxInd = i;
-      }
-      else{
-        if(data[i] > maxNum){
-          maxNum = data[i];
-          maxInd = i;
-        }
-      }
-    }
-    String maxStr = "" + maxNum;
-    int maxDigits = maxStr.length();
-    int index = 0;
-    while(index < maxDigits){
-      for(int i = 0; i < data.length; i++){
-        int temp = data[i];
-        int newTemp = temp;
-        if(index != 0){
-          newTemp = temp / ((int) Math.pow(10, index));
-        }
-        int num = newTemp % 10;
-        bucket[num].add(temp);
-      }
-      tempB.clear();
-      for(int i = 0; i < data.length; i++){
-        tempB.extend(bucket[i]);
-      }
-      index++;
-    }
-    for(int i = 0; i < data.length; i ++){
-      System.out.println(tempB.removeFront());
-      data[i] = tempB.removeFront();
-    }
+  public static void radixsort(int [] data){
+  MyLinkedList<Integer> tempB = new MyLinkedList<Integer>();
+  for(int i = 0; i < data.length; i++){
+    tempB.add(data[i]);
+    //System.out.println(tempB.get(i));
   }
-  public static void main(String[] args) {
-    int[] test = {23,4,55, 98, 80};
-    radixsort(test);
-    String newstr = "";
-    for(int i = 0; i < test.length; i++){
-      newstr += "" + test[i];
-    }
+  int maxNum = 0;
+   int maxInd = 0;
+   for(int i = 0; i < data.length; i++){
+     if(i == 0){
+       maxNum = data[i];
+       maxInd = i;
+     }
+     else{
+       if(data[i] > maxNum){
+         maxNum = data[i];
+         maxInd = i;
+       }
+     }
+   }
+   String maxStr = "" + maxNum;
+   int maxDigits = maxStr.length();
+  MyLinkedList<Integer>[] bucket = new MyLinkedList[20];
+	for(int i = 0; i < 20; i++){
+            bucket[i] = new MyLinkedList<Integer>();
   }
+  int ind = 0;
+	while(ind < maxDigits){
+	    for(int i=0;i<tempB.size();i++){
+		  int num = tempB.get(i);
+      int temp = num;
+      if(ind > 0){
+		    temp=num/((int)Math.pow(10,ind));
+      }
+      int digit = temp % 10;
+      bucket[digit].add(num);
+	    }
+	    for(int i=0;i<bucket.length;i++){
+        //System.out.println(bucket[i]);
+		    data[i] = bucket[i].removeFront();
+	    }
+	    ind++;
+	   }
+  }
+
+    public static void main(String[] args) {
+      int[] testing = {0,17,5,8,332,4,12,32,15,25,167};
+      System.out.println(Arrays.toString(testing));
+      Radix.radixsort(testing);
+      System.out.println(Arrays.toString(testing));
+    }
+
 }
